630d3f6a52f5d91452d596755a44486450f4db8e
rodrigocuadra <30705964+rodrigocuadra@users.noreply.github.com>
2023-05-27 16:03:05 -0600

    Update stress-test.sh

--- stress-test.sh
+++ stress-test.sh
@@ -421,15 +421,15 @@ exitcalls=false
 i=0
 step=0
 clear
-echo -e " *********************************************************************************************"
+echo -e " *************************************************************************************"
 	if [ "$codec" = 3 ] ;then
-		echo -e " *               Actual Test State (Protocol: "$protocol_name", Codec: "$codec_name", Recording: "$recording")              *"
+		echo -e " *           Actual Test State (Protocol: "$protocol_name", Codec: "$codec_name", Recording: "$recording")          *"
 	else
-		echo -e " *               Actual Test State (Protocol: "$protocol_name", Codec: "$codec_name", Recording: "$recording")              *"
+		echo -e " *           Actual Test State (Protocol: "$protocol_name", Codec: "$codec_name", Recording: "$recording")          *"
 	fi
-echo -e " *********************************************************************************************"
+echo -e " *************************************************************************************"
 echo -e " -------------------------------------------------------------------------------------"
-printf "%2s %7s %10s %16s %10s %10s %10s %12s %12s\n" "|" " Step |" "Calls |" "Asterisk Calls |" "CPU Load |" "Load |" "Memory |" "BW TX kb/s |" "BW RX kb/s |"
+printf "%2s %7s %10s %16s %10s %10s %12s %12s\n" "|" " Step |" "Calls |" "Asterisk Calls |" "CPU Load |" "Memory |" "BW TX kb/s |" "BW RX kb/s |"
 R1=`cat /sys/class/net/"$interface_name"/statistics/rx_bytes`
 T1=`cat /sys/class/net/"$interface_name"/statistics/tx_bytes`
 date1=$(date +"%s")
@@ -451,7 +451,6 @@ echo -e "calls, active calls, cpu load (%), memory (%), bwtx (kb/s), bwrx(kb/s),
 		bwtx="$((TKBPS/seconds))"
 		bwrx="$((RKBPS/seconds))"
 		activecalls=`asterisk -rx "core show calls" | grep "active" | cut -d' ' -f1`
-		load=`uptime | awk '{print $8}' | cut -d "," -f 1`
 		cpu=`top -n 1 | awk 'FNR > 7 {s+=$10} END {print s}'`
 		cpuint=${cpu%.*}
 		cpu="$((cpuint/numcores))"
@@ -465,8 +464,8 @@ echo -e "calls, active calls, cpu load (%), memory (%), bwtx (kb/s), bwrx(kb/s),
 		if [ "$cpu" -ge 65 ] ;then
 			echo -e "\e[91m -------------------------------------------------------------------------------------"
 		fi
-		printf "%2s %7s %10s %16s %10s %10s %12s %12s\n" "|" " "$step" |" ""$i" |" ""$activecalls" |" ""$cpu"% |" ""$load" |" ""$memory" |" ""$bwtx" |" ""$bwrx" |"
-		echo -e "$i, $activecalls, $cpu, $load, $memory, $bwtx, $bwrx, $seconds" 	>> data.csv
+		printf "%2s %7s %10s %16s %10s %10s %12s %12s\n" "|" " "$step" |" ""$i" |" ""$activecalls" |" ""$cpu"% |" ""$memory" |" ""$bwtx" |" ""$bwrx" |"
+		echo -e "$i, $activecalls, $cpu, $memory, $bwtx, $bwrx, $seconds" 	>> data.csv
 		exitstep=false
 		x=1
 		while [ $exitstep = 'false' ]  
