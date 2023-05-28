437f571ee2d3bc76ec4a0cd92742e40b69c44be1
rodrigocuadra <30705964+rodrigocuadra@users.noreply.github.com>
2023-05-28 07:37:06 -0600

    Update stress-test.sh

--- stress-test.sh
+++ stress-test.sh
@@ -447,7 +447,7 @@ echo -e "calls, active calls, cpu load (%), memory (%), bwtx (kb/s), bwrx(kb/s),
 		bwtx="$((TKBPS/seconds))"
 		bwrx="$((RKBPS/seconds))"
 		activecalls=`asterisk -rx "core show calls" | grep "active" | cut -d' ' -f1`
-		load=`uptime | awk '{print $8}' | cut -d "," -f 1`
+		load=`cat /proc/loadavg | awk '{print $0}' | cut -d " " -f 1`
 		cpu=`top -n 1 | awk 'FNR > 7 {s+=$10} END {print s}'`
 		cpuint=${cpu%.*}
 		cpu="$((cpuint/numcores))"
